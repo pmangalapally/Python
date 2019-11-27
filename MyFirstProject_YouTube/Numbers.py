db40c8b93491215eeb579aa612d7a5679cadbc89
Pavan Kumar Mangalapally <pavanmangalaplly@gmail.com>
2019-11-26 21:56:29 -0500

    Numbers added.

---
+++ MyFirstProject_YouTube/Numbers.py
@@ -0,0 +1,12 @@
+print(2*3)
+print(2-3)
+print(2%3)
+print(pow(2,3))
+print(max(4,5,6,8,1))
+print(min(4,5,6,8,1))
+print(round(3.7))
+
+from math import *
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(3.7))
