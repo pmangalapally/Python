f53cfdb90993516cdbd4fd70e2baad22b0338137
Pavan Kumar Mangalapally <pavanmangalaplly@gmail.com>
2020-03-26 22:22:56 -0400

    ex11-14 added.

---
+++ LearnPython3TheHardWay/ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+# read the WYSS section for how to run this
+script, first, second, thrird = argv
+
+print("The script is called: ", script)
+print("Your first variable is: ", first)
+print("Your second variable is: ", second)
+print("Your third variable is: ", thrird)
